17b57d1ea1f7b6580edd72baba4e64bf82a2a5b1
Amir <amir.ghandeharioon@gmail.com>
2017-10-09 23:49:56 +0000

    requestURL created

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,23 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+var GITHUB_USER = "AmirGhan";
+var GITHUB_TOKEN = "5936d38b4959dd31248f9e787cb74af98b535be0";
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  request(requestURL, function(err, response, body){
+    console.log(err);
+    //console.log(response);
+    console.log(body);
+
+  });
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
